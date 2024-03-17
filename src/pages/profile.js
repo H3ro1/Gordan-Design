b428953106c88be3d37890ce8276b78c7420d232
unknown <federicoferri14@gmail.com>
2024-03-17 18:10:35 +0700

    again

--- src/pages/profile.js
+++ src/pages/profile.js
@@ -2,13 +2,13 @@ import * as React from 'react'
 
 import Layout from '../components/layout'
 import Banner from '../components/banner'
-import Gridbody from '../components/gridbody'
+
 
 const Profile = () => {
 return(
     <Layout>
         <Banner Heading={"Hi I'm Gordan"}/>
-        <Gridbody></Gridbody>
+        
     </Layout>
     )
 }
